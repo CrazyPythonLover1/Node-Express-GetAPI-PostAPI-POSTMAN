29787c0eb1fe4c57c0d4415e4cc6c8fb6728d37e
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-09-29 14:01:13 +0600

    install express, Start a Server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+function rootcall(req, res){
+    res.send("Thank you very much")
+}
+
+app.get('/', (req, res) =>{
+    res.send("Thank you for calling me ");
+} )
+
+app.listen(3000, ()=> console.log('listening to port 3000'))
