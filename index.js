7ee975ad272581585b65534eeb9a17c6b406123a
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-09-29 15:23:52 +0600

    install nodemon , api reurn object, create multiple api

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ function rootcall(req, res){
 }
 
 app.get('/', (req, res) =>{
-    res.send("Thank you for calling me ");
-} )
+    res.send("Hello World");
+})
+
+app.get('/fruits/banana', (req, res) => {
+    res.send({fruits: "banana", quantity:20, price:60});
+})
 
 app.listen(3000, ()=> console.log('listening to port 3000'))
