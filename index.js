ec5766f936ccb81c19ca75c7c43033963ffd6d3f
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-09-29 22:10:20 +0600

    dynamic api parameter , access params , access query

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const users = [ 'asad', 'sabed', 'mainul', 'munna', 'faruqi', 'crazyPythonLover']
+
 function rootcall(req, res){
     res.send("Thank you very much")
 }
@@ -11,6 +13,13 @@ app.get('/', (req, res) =>{
 
 app.get('/fruits/banana', (req, res) => {
     res.send({fruits: "banana", quantity:20, price:60});
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(req.query.sort)
+    const name = users[id];
+    res.send({id,name});
 })
 
 app.listen(3000, ()=> console.log('listening to port 3000'))
