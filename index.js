910b010034dda1609b4e175a8a36100cca41e6b6
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-09-30 00:13:32 +0600

    post data from website and read post api response

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
 app.use(cors())
+app.use(bodyParser.json())
 
 const users = [ 'asad', 'sabed', 'mainul', 'munna', 'faruqi', 'crazyPythonLover']
 
@@ -23,6 +25,13 @@ app.get('/users/:id', (req, res) => {
     console.log(req.query.sort)
     const name = users[id];
     res.send({id,name});
+})
+
+app.post('/addUser',(req, res)=>{
+    const user = req.body;
+    user.id = 55;
+    res.send(req.body);
+    console.log("data recieved", user);
 })
 
 app.listen(3000, ()=> console.log('listening to port 3000'))
